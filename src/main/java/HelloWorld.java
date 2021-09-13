384617e95ce5f36faf630425990c3fe498870c0a
foxman-win10 <1258917637@qq.com>
2021-09-13 16:07:52 +0800

    Initial commit win10

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author foxman
+ * @Description TODO
+ * @date 2021/9/13 15:41
+ * @Copyright Gowild. All rights reserved.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World！");
+    }
+}
